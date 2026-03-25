6fa5083c527d0969f825fd2f92fbe3f3cbe0430b
cbrennan2120 <cbrennan2120@gmail.com>
2026-03-25 19:13:02 -0400

    Migrate Netlify Forms to static HTML file for Next.js Runtime v5

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,13 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased bg-rich-black text-champagne-onyx font-sans">
-        {/* Hidden form for Netlify to detect during build/crawl */}
-        <form name="prediction-form" data-netlify="true" hidden>
-          <input type="hidden" name="form-name" value="prediction-form" />
-          <input type="text" name="userName" />
-          <input type="text" name="showId" />
-          <input type="text" name="top5" />
-        </form>
+
         {children}
       </body>
     </html>
